5a7641f8fec63965d74d05ad8fe1387372dc1755
LURO-ZERO <clashluro@gmail.com>
2026-02-05 09:50:50 -0800

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
-API_ID = int(environ.get('API_ID', '')) # API ID from my.telegram.org
-API_HASH = environ.get('API_HASH', '')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
+API_ID = int(environ.get('API_ID', '12334596')) # API ID from my.telegram.org
+API_HASH = environ.get('API_HASH', '47071ddd4d2461be0a007a0f95f59fa8')  # API Hash from my.telegram.org
+BOT_TOKEN = environ.get('BOT_TOKEN', "6713190956:AAGAerjKcMQII9Nuozp9Q0s73RtvCQNor8g")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
